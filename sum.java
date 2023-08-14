3e7e4d48a4f9bcdffbb87351e583c184c0341438
Yash Dhiman <yashdhiman745@gmail.com>
2023-08-14 16:49:19 +0530

    capatilize file

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum{
     public static void main(String args[]){
         int a=5, b=3;
-        System.out.println("sum of a and b is "+(a+b));
+        System.out.println("Sum of a and b is "+(a+b));
     }
 }
